2bd8df61d00486beb6b87fdd9b53015927cce89f
zhoujing <121531845@qq.com>
2025-02-12 09:28:32 +0800

    增加metric_by_name

--- get_metric.py
+++ get_metric.py
@@ -1,9 +1,24 @@
 import os
 
-if __name__ == '__main__':
+
+def metric_by_name():
     metric_dir = 'images/'
+    for d in os.listdir(metric_dir):
+        count_num = 0
+        for root, dirs, files in os.walk(os.path.join(metric_dir, d)):
+            for file in files:
+                count_num += 1
+        print(f'{d} has {count_num} images')
+
+
+
+def metric_total(dir_path):
     count_num = 0
-    for root, dirs, files in os.walk(metric_dir):
+    for root, dirs, files in os.walk(dir_path):
         for file in files:
             count_num += 1
-    print(f'{metric_dir} has {count_num} images')
+    print(f'{dir_path} has {count_num} images')
+
+
+if __name__ == '__main__':
+    metric_total('./images/天然矿物')
